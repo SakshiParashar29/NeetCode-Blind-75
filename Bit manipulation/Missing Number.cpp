330d90564d73da4caa18f3e2c80958e0f5825336
Sakshi Kumari <sakshiparashar2910@gmail.com>
2026-06-20 16:38:42 +0530

    Create Missing Number.cpp

---
+++ Bit manipulation/Missing Number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int ans = 0;
+
+        for(int& num : nums) ans ^= num;
+
+        for(int i = 0; i <= nums.size(); i++) ans ^= i;
+
+        return ans;
+    }
+};
